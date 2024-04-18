74b3384c072fb173adf4b19e3fbefa97b6dbaad8
PuDLeZ <pudlez@gmail.com>
2024-04-17 20:38:37 -0500

    First upload of olmod-stable
    First upload of olmod-stable

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using LibGit2Sharp;
+using static System.Net.WebRequestMethods;
+
+class Program {
+    static void Main(string[] args) {
+        string gitRepo="https://github.com/overload-development-community/olmod-stable-binaries.git";
+
+        string exeDirectory = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule?.FileName ?? string.Empty) ?? string.Empty;
+        string repoPath = Path.Combine(exeDirectory, "olmod-stable-binaries");
+        string executablePath = Path.Combine(repoPath, "bin", "olmod.exe");
+
+        // Check if being asked to generate a config file
+        if (args.Contains("--config")) {
+            CreateConfigFile(exeDirectory);
+            Console.WriteLine("config.txt has been created.");
+            return; // exit
+        }
+
+        // Check if config.txt exists
+        string configFilePath = Path.Combine(exeDirectory, "config.txt");
+
+        // set default to autoclose the window
+        bool autoCloseWindow = true;
+
+        if (System.IO.File.Exists(configFilePath))
+        { // Fully qualify the File class
+          // Read lines from config.txt and pass arguments to the executable
+            string[] lines = System.IO.File.ReadAllLines(configFilePath); // Fully qualify the File class
+            foreach (string line in lines)
+            {
+                if (!string.IsNullOrWhiteSpace(line) && !line.TrimStart().StartsWith("#"))
+                {
+                    if (line.StartsWith("auto-close-window=false"))
+                    {
+                        if (line.StartsWith("auto-close-window=false"))
+                        {
+                            autoCloseWindow = false;
+                        }
+                    }
+                    else
+                    {
+                        string[] argsToAdd = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        args = args.Concat(argsToAdd).ToArray();
+                    }
+                }
+            }
+        }
+
+        // Check if the repo exists locally
+        if (!Repository.IsValid(repoPath)) {
+            // doesn't exist so lets clone
+            Console.WriteLine("Grabbing the latest olmod version...");
+            Repository.Clone(gitRepo, repoPath);
+        } else {
+            // exists so lets pull
+            using (var repo = new Repository(repoPath)) {
+                Console.WriteLine("Making sure we have the latest olmod version...");
+                Commands.Pull(repo, new Signature("Anonymous", "anonymous@otl.gg", DateTimeOffset.Now), new PullOptions());
+            }
+        }
+
+        // launch olmod
+        Console.WriteLine("Launching the latest olmod version...");
+        Process.Start(executablePath, string.Join(" ", args));
+
+        // if autoCloseWindow is set to false, wait for user input before closing
+        if (!autoCloseWindow) {
+            Console.WriteLine("Press any key to close...");
+            Console.ReadKey();
+        }
+    }
+
+    // ********************************
+    // * Create a generic config file *
+    // ********************************
+    static void CreateConfigFile(string directory)
+    {
+        string configFilePath = Path.Combine(directory, "config.txt");
+        if (!System.IO.File.Exists(configFilePath))
+        { // Fully qualify the File class
+            using (StreamWriter writer = System.IO.File.CreateText(configFilePath))
+            { // Fully qualify the File class
+                writer.WriteLine("#  Config file for olmod-stable");
+                writer.WriteLine("#  Lines that begin with # are ignored...");
+                writer.WriteLine("#");
+                writer.WriteLine("#  Uncomment the following line to keep the console window open");
+                writer.WriteLine("#auto-close-window=false");
+                writer.WriteLine("#");
+                writer.WriteLine("#  Each argument should be on a separate line");
+                writer.WriteLine("#-gamedir \"C:\\Program Files (x86)\\Steam\\steamapps\\common\\Overload\"");
+                writer.WriteLine("#-frametime");
+            }
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # olmod-stable
-Small C# program to make sure you're using the latest "stable" version of olmod
+Small C# program to make sure you're using the latest "stable" version of olmod without any extra bells and whistles.
+
+
+### Usage
+-----
+To use: download the latest version from the releases page or compile it yourself. Then load olmod-stable.exe and enjoy having the latest olmod version!
+
+
+If you want to keep the window open or pass command line arguments to olmod, edit the config.txt file. If you deleted the config.txt file, you can generate a new generic one by executing: olmod-stable.exe --config
+This will create a new config.txt file with the following entries
+
+```
+#  Config file for olmod-stable
+#  Lines that begin with # are ignored...
+#
+#  Uncomment the following line to keep the console window open
+#auto-close-window=false
+#
+#  Each argument should be on a separate line
+#-gamedir "C:\Program Files (x86)\Steam\steamapps\common\Overload"
+#-frametime
+
+```
